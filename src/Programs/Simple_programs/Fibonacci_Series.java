142aa8aa76e6a9317a91c9db0b6bea52332ca75c
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-07-26 15:56:23 +0530

    comments

---
+++ src/Programs/Simple_programs/Fibonacci_Series.java
@@ -0,0 +1,17 @@
+package Programs.Simple_programs;
+
+public class Fibonacci_Series {
+    public static void main(String[] args) {
+        int count=10;
+        int n1=0;
+        int n2=1;
+        System.out.print(n1+" "+n2+" ");
+        for (int i=2; i<count; i++){
+            int n3=n1+n2;
+            System.out.print(n3+" ");
+            n1=n2;
+            n2=n3;
+
+        }
+    }
+}
