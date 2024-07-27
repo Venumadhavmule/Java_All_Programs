84d06f67f104a0aff57afb30e6bb78e9515750e4
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-07-27 09:38:15 +0530

    Fibonacci_Series

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    static int d=65;
+    public static void add(int a, int b){
+        int c= a+b;
+        System.out.println(c);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        add(10,20);
+        System.out.println(d);
     }
 }
