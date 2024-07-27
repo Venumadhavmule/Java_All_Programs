f1efd0f852df4d90132ad1e66bd4ee1ef741a180
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-07-27 09:51:32 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-    static int d=65;
+    static int c;
     public static void add(int a, int b){
-        int c= a+b;
+        c= a+b;
         System.out.println(c);
     }
     public static void main(String[] args) {
 //        System.out.println("Hello world!");
 
         add(10,20) ;
-        System.out.println(d);
+        System.out.println(c);
     }
 }
