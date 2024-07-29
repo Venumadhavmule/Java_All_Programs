3de66f63ebbec987f824f99d8bdccd03a92eb67b
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-07-29 14:25:27 +0530

    noOfBicycles.java

--- src/Lecture/OOPs/Bicycle.java
+++ src/Lecture/OOPs/Bicycle.java
@@ -4,13 +4,20 @@ class Bicycle {
     private int cadence;
     private int speed; 
     private int gear;
-    public Bicycle(){
-        System.out.println(cadence);
-    }
+    private int id;
+    private static int numberOfBicycles=0;
+//    public Bicycle(){
+//        System.out.println("Default constructor is invoked");
+//        System.out.println(cadence);
+//    }
     public Bicycle(int startCadence, int startSpeed,int startGear){
         cadence=startCadence;
         speed=startSpeed;
         gear=startGear;
+        id =++numberOfBicycles;
+    }
+    public int getNumberOfBicycles(){
+        return numberOfBicycles;
     }
     public int getCadence(){
         return cadence;
@@ -36,12 +43,13 @@ class Bicycle {
 }
 class BicycleDemo{
     public static void main(String args[]){
-        Bicycle b= new Bicycle();
+//        Bicycle b= new Bicycle();
         Bicycle b1= new Bicycle(50,60,5);
         Bicycle b2=new Bicycle(30,40,4);
         System.out.println(b1.getCadence());
         System.out.println(b1.getSpeed());
         System.out.println(b1.getGear());
+        System.out.println("Number of Bicycles :"+b1.getNumberOfBicycles());
 //        b1.changeCadence(50);
 //        b1.changeGear(2);
 //        b1.speedUp(60);
