44e388a294681e0780f999706bb62310243a1d3f
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-04 23:16:49 +0530

    Commented

--- src/Patterns/Symmetric_Triangle.java
+++ src/Patterns/Symmetric_Triangle.java
@@ -1,5 +1,5 @@
 package Patterns;
-/* for print the following symmetric pattern
+/* program print the following symmetric pattern
         *
         **
         ***
@@ -15,7 +15,12 @@ public class Symmetric_Triangle {
     public static void main(String[] args) {
         int n=5;
         for(int i=1; i<=2*n-1;i++){
+
+            //take starting value
             int start=i;
+
+            //update loop start value if it exceeds n value
+
             if(i>n) start=2*n -i;
             for(int j=1; j<=start;j++){
                 System.out.print("*");
