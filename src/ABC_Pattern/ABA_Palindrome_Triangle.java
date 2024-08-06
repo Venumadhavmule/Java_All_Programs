c6078829d6a2f4c84a6e48af43442bfbf0079af7
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-06 11:06:41 +0530

    ABC plaindrome Traingle

--- src/ABC_Pattern/ABA_Palindrome_Triangle.java
+++ src/ABC_Pattern/ABA_Palindrome_Triangle.java
@@ -1,6 +1,40 @@
 package ABC_Pattern;
+/*program for
+_ _ _ _ A _ _ _ _
+_ _ _ A B A _ _ _
+_ _ A B C B A _ _
+_ A B C D C B A _
+A B C D E D C B A
+
+ */
 public class ABA_Palindrome_Triangle {
     public static void main(String args[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+
+            //loop for first spaces
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("_ ");
+            }
+
+            //for middle palindrome pattern
+            int breakPoint = (2*i+1)/2;
+            char ch='A';
+            for(int j=1; j<=(2*i)+1; j++){
+                System.out.print(ch+" ");
+                if(j<=breakPoint){
+                    ch++;
+                }else {
+                    ch--;
+                }
+            }
+
+            //loop for last spaces
+            for(int j=0;j<n-i-1;j++){
+                System.out.print("_ ");
+            }
+            System.out.println();
+        }
 
     }
 }
--- src/Patterns/RightAngledTriangle.java
+++ src/Patterns/RightAngledTriangle.java
@@ -1,5 +1,13 @@
 package Patterns;
+/*
+        *
+        *	*
+        *	*	*
+        *	*	*	*
+        *	*	*	*	*
+        *	*	*	*	*	*
 
+ */
 public class RightAngledTriangle {
     public static void main(String [] args) {
         for(int i = 0;i <= 5; i++) {
