5e99669e312a5f5f898ec01d30157bb44a7197c2
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-06 08:02:05 +0530

    ABC_Left_Upper_Right_Angle_triangle.java

--- src/ABC_Pattern/ABBCCC_Right_Angled_Triangle.java
+++ src/ABC_Pattern/ABBCCC_Right_Angled_Triangle.java
@@ -10,9 +10,8 @@ package ABC_Pattern;
 public class ABBCCC_Right_Angled_Triangle {
     public static void main(String args[]){
         for(int i=0; i<5; i++){
-
+            char ch=(char)('A'+i);
             for(int j=0;j<=i;j++){
-                char ch=(char)('A'+i);
                 System.out.print(ch+" ");
             }
             System.out.println();
