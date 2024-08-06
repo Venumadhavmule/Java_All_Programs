c0a89d9d029f279e0108a554304ca3bda83f20e8
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-06 22:44:55 +0530

    Reverse_Pyramid

---
+++ src/Patterns/Reverse_Pyramid.java
@@ -0,0 +1,35 @@
+package Patterns;
+/*program for
+
+*****************
+ ***************
+  *************
+   ***********
+    *********
+     *******
+      *****
+       ***
+        *
+
+ */
+import java.util.Scanner;
+public class Reverse_Pyramid {
+    public static void main(String[] args) {
+        Scanner ref=new Scanner(System.in);
+        System.out.print("Enter n value: ");
+        int n=ref.nextInt(); // Number of rows for the pyramid
+        for(int i=0;i<n;i++){
+
+            // Print leading spaces
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for(int j=0;j<(n*2-i*2)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
