1f91bf6540eb0f59de56ddba0cbeb0e2a2fd99b3
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-06 07:47:49 +0530

    ABC_Left_Upper_Right_Angle_triangle.java

---
+++ src/ABC_Pattern/ABC_Left_Upper_Right_Angle_triangle.java
@@ -0,0 +1,19 @@
+package ABC_Pattern;
+/* program for
+    A B C D E
+    A B C D
+    A B C
+    A B
+    A
+
+ */
+public class ABC_Left_Upper_Right_Angle_triangle {
+    public static void main(String arg[]){
+        for(int i=0; i<5;i++){
+            for(char ch='A'; ch<='A'+(5-i-1);ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
