849a8e95475bf6631660e6e284c58bce98236353
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-07 10:41:39 +0530

    PalindromeTriangular.java

--- src/NumbersPattern/PalindromeTriangular.java
+++ src/NumbersPattern/PalindromeTriangular.java
@@ -1,20 +1,36 @@
 package NumbersPattern;
 
+/*
+     1
+    212
+   32123
+  4321234
+ 543212345
+65432123456
+
+ */
 import java.util.Scanner;
 public class PalindromeTriangular {
     public static void main(String args[]){
-//        Scanner ref= new Scanner(System.in);
-//        System.out.print("Enter N value: ");
-//        int n= ref.nextInt();
-        for(int i=0;i<5;i++){
+        Scanner ref= new Scanner(System.in);
+        System.out.print("Enter N value: ");
+        int n= ref.nextInt();
+        for(int i=0;i<n;i++){
             //loop for first spaces
-            for(int j=0;j<5-i-1;j++){
+            for(int j=0;j<n-i-1;j++){
                 System.out.print(" ");
             }
 
+            int breakpoint=(2*i+1)/2;
+            int present=i+1;
             //loop for pattern
             for(int j=0; j<2*i+1; j++){
-                System.out.print(i);
+                System.out.print(present);
+                if(j<breakpoint){
+                    present--;
+                }else {
+                    present++;
+                }
             }
             System.out.println();
         }
