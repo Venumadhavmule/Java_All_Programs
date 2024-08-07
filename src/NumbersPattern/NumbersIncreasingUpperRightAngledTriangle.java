84dbc236c621531792e0653ded52eae70acf8cd1
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-07 10:52:11 +0530

    Name Update

--- src/NumbersPattern/NumbersIncreasingUpperRightAngledTriangle.java
+++ src/NumbersPattern/NumbersIncreasingUpperRightAngledTriangle.java
@@ -8,7 +8,7 @@ package NumbersPattern;
 
  */
 
-public class NumbersIncreasingReversePyramid {
+public class NumbersIncreasingUpperRightAngledTriangle {
     public static void main(String [] args) {
         for(int i = 0; i <= 5; i++){
             for(int j = 1; j<=5-i; j++){
