04af9bb51e2d2492553aee105ff8a954d8d39301
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-12 11:49:06 +0530

    Diamond_Surrounded_Stars

--- src/StarPatterns/Diamond_Surrounded_Stars.java
+++ src/StarPatterns/Diamond_Surrounded_Stars.java
@@ -1,2 +1,59 @@
-package StarPatterns;public class Diamond_Surrounded_Stars {
+package StarPatterns;
+/* program for
+ **********
+ ****  ****
+ ***    ***
+ **      **
+ *        *
+ *        *
+ **      **
+ ***    ***
+ ****  ****
+ **********
+
+ */
+
+public class Diamond_Surrounded_Stars {
+    public static void main(String args[]){
+        int spaces=0;
+        //1.... for upper part
+        for(int i=0;i<5;i++){
+            //for first stars
+            for(int j=0;j<5-i;j++){
+                System.out.print("*");
+            }
+            //for spaces
+            for(int j=0;j<spaces;j++){
+                System.out.print(" ");
+            }
+            spaces+=2;
+
+            //for 2nd Stars
+            for(int j=0;j<5-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //2... for lower part
+        for(int i=0;i<5;i++){
+            //for first star part
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+
+            //for spaces
+            spaces-=2;
+            for(int j=0;j<spaces;j++){
+                System.out.print(" ");
+            }
+
+
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
