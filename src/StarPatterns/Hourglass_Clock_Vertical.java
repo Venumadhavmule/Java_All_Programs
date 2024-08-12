b37051be3008f165e8a8155ef3bbe6d4b4c55bb1
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-13 00:12:52 +0530

    Update

--- src/StarPatterns/Hourglass_Clock_Vertical.java
+++ src/StarPatterns/Hourglass_Clock_Vertical.java
@@ -1,17 +1,35 @@
 package StarPatterns;
 /* program for
 
- *        *
- **      **
- ***    ***
- ****  ****
- **********
- ****  ****
- ***    ***
- **      **
- *        *
+Enter n value: 6
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
+
+By using Second Method
+
+*          *
+**        **
+***      ***
+****    ****
+*****  *****
+************
+*****  *****
+****    ****
+***      ***
+**        **
+*          *
 
  */
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 public class Hourglass_Clock_Vertical {
     public static void main(String[] args) {
@@ -56,5 +74,30 @@ public class Hourglass_Clock_Vertical {
             }
             System.out.println();
         }
+        System.out.println("\nBy using Second Method\n");
+        int sSpaces=2*n-2;
+        ////2nd Method
+        for(int i=1;i<=2*n-1;i++){
+            int stars=i;
+            if(i>n) stars=2*n-i;
+            //stars
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+
+            //spaces
+            for(int j=1;j<=sSpaces;j++){
+                System.out.print(" ");
+            }
+
+            //stars
+            for(int j=1;j<=stars;j++){
+                System.out.print("*");
+            }
+            if(i<n) sSpaces -=2;
+            else sSpaces +=2;
+            System.out.println();
+        }
+
     }
 }
