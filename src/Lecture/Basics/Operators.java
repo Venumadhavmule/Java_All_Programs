1b9822327828e9b5abb0291839418dea6adcd2ef
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-08-16 09:54:34 +0530

    Operators.java

--- src/Lecture/Basics/Operators.java
+++ src/Lecture/Basics/Operators.java
@@ -1,15 +1,19 @@
 package Lecture.Basics;
 
 public class Operators{
+    static final double PI=3.14;
     public static void main(String [] args) {
         int x = 10;
-        Number n=12;
-        System.out.println("n value: "+n);
-        n=56.9999985;
-        System.out.println("n value: "+n);
-        n=152.6f;
-        System.out.println("n value: "+n);
+        Number n = 12;
+        System.out.println(PI);
+        System.out.println("n value: " + n);
+        n = 56.9999985;
+        System.out.println("n value: " + n);
+        n = 152.6f;
+        System.out.println("n value: " + n);
         System.out.println(++x);
         System.out.println(x);
+
     }
+
 }
