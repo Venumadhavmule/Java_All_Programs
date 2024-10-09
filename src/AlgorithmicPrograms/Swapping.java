b5d05fb2d648a40f93a95129114d3a0ebcd68b64
Venumadhava Reddy Mule <130583741+Venumadhavmule@users.noreply.github.com>
2024-10-09 09:49:56 +0530

    Update Swapping.java

--- src/AlgorithmicPrograms/Swapping.java
+++ src/AlgorithmicPrograms/Swapping.java
@@ -2,7 +2,7 @@ package AlgorithmicPrograms;
 
 //Swapping of two numbers without using 3Rd variable
 public class Swapping {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int a = 10;
         int b = 20;
         System.out.println("Before Swapping\n a :"+a+" b :"+b);
