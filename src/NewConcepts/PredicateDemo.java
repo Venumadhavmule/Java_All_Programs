c45d527c9ba83267e51a452c19beea29efa365c3
Venumadhava Reddy <venumadhavamule@gmail.com>
2025-10-02 23:55:18 +0530

    PredicateDemo

--- src/NewConcepts/PredicateDemo.java
+++ src/NewConcepts/PredicateDemo.java
@@ -17,8 +17,28 @@ package NewConcepts;
  */
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class PredicateDemo {
     public static void main(String[] args) {
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        Predicate<String> isNotEmpty = String::isEmpty;
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        System.out.println("Even Numbers");
+        numbers.stream()
+                .filter(isEven)
+                .forEach(System.out::println);
+
+        String test = "Hello";
+        if (isNotEmpty.test(test)) {
+            System.out.println(test + " is not Empty");
+        } else {
+            System.out.println(test + " is Empty");
+        }
 
     }
 }
