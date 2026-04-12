2fc0274517de4e6b2688414db7c1e36a6bf8044d
Venumadhava Reddy Mule <venumadhavamule@gmail.com>
2026-04-12 15:02:40 +0530

    Filtering LIsts and Arrays with lambda functions

--- src/Streams_Lambdas/Filtering_List_with_Lambda.java
+++ src/Streams_Lambdas/Filtering_List_with_Lambda.java
@@ -1,21 +1,38 @@
 package Streams_Lambdas;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Filtering_List_with_Lambda {
-    public static void main(String[] args) {
-        String names[] = {"ajay", "Banu", "Chinni", "Dolly", "Eagle"};
-        String villages[] = new String[6];
-        villages[0] = "Thimmapalem";
-        villages[1] = "Agraharam";
-        villages[2] = "Ongole";
-        villages[3] = "China";
-        villages[4] = "Mexico";
-        villages[5] = "thimmapalem";
 
-        List<String> filteredList = Arrays.stream(villages).filter(it -> it.toLowerCase().startsWith("t")).toList();
-        filteredList.forEach(System.out::println);
+    public static void main(String args[]){
+
+        String movies[] = new String[8];
+        movies[0] = "Mirchi";
+        movies[1] = "Rebel";
+        movies[2] = "Bunny";
+        movies[3] = "Maharshi";
+        movies[4] = "Dhruva";
+        movies[5] = "KGF";
+        movies[6] = "Salar";
+        movies[7] = "Agent";
+
+        int ages[] = new int[3];
+        ages[0] = 55;
+        ages[1] = 01;
+        ages[2] = 22;
+
+//        List<String> filteredMovies = Arrays.stream(movies).filter(movie -> movie!= null).sorted().toList();
+//        filteredMovies.forEach(System.out::println);
+
+//        Arrays.stream(movies).sorted().map(it -> it.toUpperCase()).toList().forEach(System.out::println);
+
+        List<String> filteredMovies = Arrays.stream(movies).map(String::toLowerCase).filter(it -> it.contains("s")).toList();
+        filteredMovies.forEach(System.out::println);
+//        Stream.of(movies).filter(movie -> movie != null).sorted().forEach(System.out::println);
+//        Arrays.stream(ages).boxed().sorted((a, b) ->b-a ).forEach(System.out::println);
 
     }
 }
