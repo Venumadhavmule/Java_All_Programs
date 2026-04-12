2092b4723ed80c287c90b3a8c3cd38d784ae5e18
Venumadhava Reddy Mule <venumadhavamule@gmail.com>
2026-04-12 15:02:00 +0530

    Functional Interface update

---
+++ src/Streams_Lambdas/Calculator.java
@@ -0,0 +1,9 @@
+package Streams_Lambdas;
+
+interface Calculator{
+    int calculate(int a, int b);
+
+//    default double calculate(double a, double b){
+//        return 00.0;
+//    };
+}
--- src/Streams_Lambdas/Custom_Functional_Interface.java
+++ src/Streams_Lambdas/Custom_Functional_Interface.java
@@ -1,19 +1,37 @@
 package Streams_Lambdas;
 
+
+//    @FunctionalInterface
+
 public class Custom_Functional_Interface {
+    public static void main(String args[]) {
 
-    @FunctionalInterface
-    interface Calculator {
-        int calculate(int a, int b);
+        Calculator add = new Calculator() {
+            @Override
+            public int calculate(int a, int b) {
+                System.out.println("Addition: "+(a+b));
+                return a+b;
+            }
+        };
+
+            Calculator multiply = (a, b) -> {
+                System.out.println("Multiply: "+(a*b));
+                return a * b;
+            };
+
+            Calculator subtraction = (a, b) -> a - b;
+            Calculator division = (a, b) -> {
+                if(b == 0) throw new ArithmeticException("Cannot divided by 0");
+
+                return a / b;
+            };
+
+            add.calculate(22,769);
+            multiply.calculate(567,694);
+        System.out.println("Division: "+division.calculate(33673,55));
+
+
+
     }
 
-    public static void main(String[] args) {
-        Calculator add = (a, b) -> a + b;
-        Calculator multiply = ((a, b) -> a * b);
-        Calculator subtraction = ((a, b) -> a - b);
-
-        System.out.println("Addition: " + add.calculate(2, 5));
-        System.out.println("Multiplication: " + multiply.calculate(2, 6));
-        System.out.println("Subtraction: " + subtraction.calculate(50, 22));
-    }
 }
